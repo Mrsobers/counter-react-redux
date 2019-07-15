e63ceb2151da6dc42a5f94599e9148a068bf0b28
Mrsobers <3480104650@qq.com>
2019-07-15 15:08:52 +0800

    use redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import {counter} from './reducers'
 import {createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
                                   //应用异步中间件  
-const store = createStore(counter,applyMiddleware(thunk));
+const store = createStore(
+    counter,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store
